2e40bc88a0273f0f2f1625759ff9af5c86f3d9cc
Calvin Wijaya <47976609+crshao@users.noreply.github.com>
2021-01-23 15:20:57 +0700

    header done

--- src/Header.js
+++ src/Header.js
@@ -2,19 +2,22 @@ import React from 'react'
 import "./Header.css";
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from "@material-ui/core/IconButton"
+import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
     return (
         <div className="header">
-            <h1>This is a header</h1>
             <IconButton>
                 <PersonIcon fontSize="large" className="header__icon"/>
             </IconButton>
             <img
                 className="header__logo"
-                src="C:\Users\hao\Documents\Project\tinder-clone-mern\media\images\tinderlogo.png"
+                src="https://i.dlpng.com/static/png/6423370_preview.png"
                 alt=""
             />
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon"/>
+            </IconButton>
         </div>
     )
 }
